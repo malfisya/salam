c86cbcb0dbfead918f8597c7b32cc519c8dc0143
Muhammad Alfi Syahrin <malfisya.dev@hotmail.com>
2025-08-25 15:55:02 +0700

    Refactor codebase
    Breakt the component into smaller chunks. Hopefully making it more
    modular.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,24 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use std::process::Command;
+
+use slint::ComponentHandle;
 slint::include_modules!();
 
 fn main() {
     let app = MainWindow::new().unwrap();
 
-    app.on_close(move || {
+    app.global::<Callbacks>().on_close(move || {
         std::process::exit(0);
     });
 
-    app.on_openUrl(move |url| {
+    app.global::<Callbacks>().on_openUrl(move |url| {
         if let Err(err) = open::that(url.as_str()) {
             eprintln!("Failed to open link: {err}");
         }
     });
 
-    app.on_startInstaller(|| {
+    app.global::<Callbacks>().on_startInstaller(|| {
         if let Err(err) = Command::new("calamares-gui").spawn() {
             eprintln!("Failed to launch installer: {err}");
         }
